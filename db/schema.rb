1f61462f51d3c17e9da3d8d0cc5dcdbaef02cae6
asaoka-yuito <yyy>
2023-09-13 21:35:09 +0900

    add: 画像アップローダーの実装(掲示板)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_13_063235) do
+ActiveRecord::Schema.define(version: 2023_09_13_121449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_09_13_063235) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "post_image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
