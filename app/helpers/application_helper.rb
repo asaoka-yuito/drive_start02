72f2503449798f8b8dd812f6c49bb42292bd071d
asaoka-yuito <yyy>
2023-09-15 16:29:16 +0900

    add: タイトルを動的に出力

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # タイトルをページごとに変える
+  def page_title(page_title = '')
+    base_title = 'DriveStart02'
+
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
