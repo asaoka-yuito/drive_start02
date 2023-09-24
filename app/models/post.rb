3bae37c42dd71c4c008001cbf84c6be0d5fd1ed0
asaoka-yuito <cybppxbkg@gmail.com>
2023-09-24 23:45:10 +0900

    add: 掲示板の検索機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,15 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
 	validates :body, presence: true, length: { maximum: 65_535 }
+
+
+  # Ransackv4.0.0から属性と関連付けの明示的な許可が必要になった（許可するカラムを書く）
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "post_image", "title", "updated_at", "user_id"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["comments", "user"]
+  end
+
 end
