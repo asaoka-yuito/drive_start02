da8b587811dc2657eb786f16196f28294c76221b
asaoka-yuito <cybppxbkg@gmail.com>
2023-09-30 22:54:15 +0900

    add: 画像プレビューの実装（post_form）

--- app/javascript/src/common.js
+++ app/javascript/src/common.js
@@ -1,5 +1,7 @@
+// 関数に「window.」を付与し、グローバル変数として定義する事により処理が実行された
+
 window.previewImage = function(id) {
-  const target = this.event.target;
+  const target = this.target;
   const file = target.files[0];
   const reader  = new FileReader();
   reader.onloadend = function () {
