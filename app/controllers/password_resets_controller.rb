c6d2f1409bcfeff8f1d15ab115535fb30cd94f71
asaoka-yuito <cybppxbkg@gmail.com>
2024-05-06 21:04:53 +0900

    update: 一部修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -23,7 +23,7 @@ class PasswordResetsController < ApplicationController
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-    return not_authenticated if @user.blank?
+    not_authenticated if @user.blank?
   end
       
   # ユーザーがパスワードのリセットフォームを送信したときに発生
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,11 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.reset_password_email.subject
-  #
-  # パスワードリセット修正必要箇所
-  def reset_password_email
-    # メールの送信元のアドレスを指定できる
-    default from: 'from@example.com'
-    def reset_password_email
-      @user = User.find(user.id)
-      @url = edit_password_reset_url(@user.reset_password_token)
-
+  # メールの送信元のアドレスを指定できる
+  default from: 'from@example.com'
+  
+  def reset_password_email(user)
+    @user = User.find(user.id)
+    @url = edit_password_reset_url(@user.reset_password_token)
       # メールの宛先、件名を指定
       mail(to: user.email, subject: 'パスワードリセット')
-    end
-  end
+  end  
 end
